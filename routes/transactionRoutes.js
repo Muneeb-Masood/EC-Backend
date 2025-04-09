d5da059ad4b846da006b25849448f125cd307016
Muneeb Masood <muneebned844@gmail.com>
2025-04-09 23:56:00 +0500

    Delete the files of ML Model API's, completed in previous PR

--- routes/transactionRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { transactions } = require('../controllers/kycControllerDocUpload');
-
-router.post('/uploadDocs', transactions);
-module.exports = router;
